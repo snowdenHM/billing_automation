b7907b48868b3680388081e3dce2733bf6af9f33
Heramb Mishra <herambmishra@geniusvision.in>
2025-08-15 01:45:44 +0530

    Production Update

--- config/settings/production.py
+++ config/settings/production.py
@@ -38,3 +38,11 @@ LOGGING["root"]["level"] = "WARNING"
 AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",        # REQUIRED for admin
 ]
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+]
+
