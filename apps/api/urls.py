ee238fad01075e01acd5e64be1939f25d5e27139
Heramb Mishra <herambmishra@geniusvision.in>
2025-09-01 00:21:06 +0530

    Urls Fix

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path("users/", include("apps.users.urls", namespace="users")),
+    path("", include("apps.users.urls", namespace="users")),
     path("", include("apps.organizations.urls")),
-    path("plan/", include("apps.subscriptions.urls")),
+    path("", include("apps.subscriptions.urls")),
     path("zoho/", include("apps.module.zoho.urls")),
     path("tally/", include("apps.module.tally.urls")),
-    path("dashboard/", include("apps.dashboard.urls", namespace="dashboard")),
+    path("", include("apps.dashboard.urls", namespace="dashboard")),
 
 ]
