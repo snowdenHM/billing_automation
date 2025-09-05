51ee3f1147ef57c4dfc3df9b75bdd1211ffd6c50
Heramb Mishra <herambmishra@geniusvision.in>
2025-09-05 23:02:06 +0530

    Key Configure

--- config/settings/base.py
+++ config/settings/base.py
@@ -19,7 +19,7 @@ if ENV_FILE.exists():
 SECRET_KEY = env("SECRET_KEY", default="django-insecure-change-me")
 DEBUG = env.bool("DEBUG", default=False)
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*", "localhost:5173", "34.51.42.227"])
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "your_openai_api_key_here")
+OPENAI_API_KEY = env.get_value("OPENAI_API_KEY", "your_openai_api_key_here")
 
 
 # ------------------------------------------------------------
