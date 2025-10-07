a8102ac16f4decf38e352f814e64b549cd8dbece
Heramb <heramb1008@gmail.com>
2025-10-07 22:44:53 +0530

    Tally App Admin Update

--- apps/module/tally/admin.py
+++ apps/module/tally/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from django.http import JsonResponse
+from django.urls import path, reverse
+from django.utils.safestring import mark_safe
 
 from .models import (
     ParentLedger,
@@ -19,14 +22,47 @@ class ParentLedgerAdmin(admin.ModelAdmin):
     list_filter = ('organization', 'created_at')
     search_fields = ('parent', 'organization__name')
     readonly_fields = ('created_at', 'updated_at')
+    autocomplete_fields = ('organization',)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "organization":
+            # Only show organizations that the user has access to
+            if not request.user.is_superuser:
+                kwargs["queryset"] = request.user.organizations.all()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 class LedgerAdmin(admin.ModelAdmin):
     list_display = ('name', 'parent', 'master_id', 'alter_id', 'organization', 'created_at')
-    list_filter = ('parent', 'organization', 'created_at')
+    list_filter = ('organization', 'parent', 'created_at')
     search_fields = ('name', 'master_id', 'parent__parent', 'organization__name')
     readonly_fields = ('created_at', 'updated_at')
-    autocomplete_fields = ('parent',)
+    autocomplete_fields = ('organization',)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "parent":
+            # Filter parent ledgers by organization if editing existing object
+            obj_id = request.resolver_match.kwargs.get('object_id')
+            if obj_id:
+                try:
+                    ledger = Ledger.objects.get(pk=obj_id)
+                    kwargs["queryset"] = ParentLedger.objects.filter(organization=ledger.organization)
+                except Ledger.DoesNotExist:
+                    pass
+            else:
+                # For new objects, get organization from GET params
+                org_id = request.GET.get('organization')
+                if org_id:
+                    kwargs["queryset"] = ParentLedger.objects.filter(organization_id=org_id)
+                else:
+                    kwargs["queryset"] = ParentLedger.objects.none()
+        elif db_field.name == "organization":
+            if not request.user.is_superuser:
+                kwargs["queryset"] = request.user.organizations.all()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    class Media:
+        js = ('admin/js/dependent_dropdown.js',)
 
 
 class TallyConfigAdmin(admin.ModelAdmin):
@@ -42,11 +78,12 @@ class TallyConfigAdmin(admin.ModelAdmin):
     list_filter = ('organization',)
     search_fields = ('organization__name',)
     ordering = ('organization__name',)
+    autocomplete_fields = ('organization',)
 
     def get_queryset(self, request):
         """Filter queryset based on user permissions and prefetch related data"""
         qs = super().get_queryset(request)
-        return qs.select_related('organization').prefetch_related(
+        qs = qs.select_related('organization').prefetch_related(
             'igst_parents',
             'cgst_parents',
             'sgst_parents',
@@ -55,13 +92,41 @@ class TallyConfigAdmin(admin.ModelAdmin):
             'chart_of_accounts_expense_parents'
         )
 
+        # Filter by user organization if not superuser
+        if not request.user.is_superuser:
+            qs = qs.filter(organization__in=request.user.organizations.all())
+
+        return qs
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('get-parent-ledgers/', self.admin_site.admin_view(self.get_parent_ledgers),
+                 name='tally_tallyconfig_get_parent_ledgers'),
+        ]
+        return custom_urls + urls
+
+    def get_parent_ledgers(self, request):
+        """AJAX endpoint to get parent ledgers for an organization"""
+        org_id = request.GET.get('org_id')
+        if org_id:
+            parent_ledgers = ParentLedger.objects.filter(organization_id=org_id).values('id', 'parent')
+            return JsonResponse({'parent_ledgers': list(parent_ledgers)})
+        return JsonResponse({'parent_ledgers': []})
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "organization":
+            if not request.user.is_superuser:
+                kwargs["queryset"] = request.user.organizations.all()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
     def formfield_for_manytomany(self, db_field, request, **kwargs):
         """Filter ManyToMany fields by organization context"""
-        if hasattr(request, '_obj_'):
+        if hasattr(request, '_obj_') and request._obj_ is not None:
             # If editing existing object, filter by its organization
             org = request._obj_.organization
         else:
-            # For new objects, try to get organization from GET params or user
+            # For new objects, try to get organization from GET params
             org_id = request.GET.get('organization')
             if org_id:
                 try:
@@ -76,13 +141,28 @@ class TallyConfigAdmin(admin.ModelAdmin):
                                    'vendor_parents', 'chart_of_accounts_parents',
                                    'chart_of_accounts_expense_parents']:
             kwargs["queryset"] = ParentLedger.objects.filter(organization=org)
+        elif db_field.name in ['igst_parents', 'cgst_parents', 'sgst_parents',
+                              'vendor_parents', 'chart_of_accounts_parents',
+                              'chart_of_accounts_expense_parents']:
+            # Show empty queryset if no organization is selected
+            kwargs["queryset"] = ParentLedger.objects.none()
 
         return super().formfield_for_manytomany(db_field, request, **kwargs)
 
     def get_form(self, request, obj=None, **kwargs):
         """Store the object in request for use in formfield_for_manytomany"""
         request._obj_ = obj
-        return super().get_form(request, obj, **kwargs)
+        form = super().get_form(request, obj, **kwargs)
+
+        # Add custom JavaScript for dependent dropdowns
+        form.Media.js = form.Media.js + ('admin/js/tally_dependent_dropdown.js',)
+
+        return form
+
+    def render_change_form(self, request, context, *args, **kwargs):
+        """Add organization change URL to context for JavaScript"""
+        context['get_parent_ledgers_url'] = reverse('admin:tally_tallyconfig_get_parent_ledgers')
+        return super().render_change_form(request, context, *args, **kwargs)
 
     def display_mappings(self, obj):
         """Display a summary of the number of ledger mappings"""
@@ -152,7 +232,11 @@ class TallyConfigAdmin(admin.ModelAdmin):
         }),
     )
 
+    class Media:
+        js = ('admin/js/tally_dependent_dropdown.js',)
 
+
+# Enhanced admin classes for other models with organization filtering
 class TallyVendorAnalyzedProductInline(admin.TabularInline):
     model = TallyVendorAnalyzedProduct
     extra = 0
@@ -166,6 +250,8 @@ class TallyVendorAnalyzedBillAdmin(admin.ModelAdmin):
     search_fields = ('bill_no', 'vendor__name', 'selected_bill__bill_munshi_name')
     readonly_fields = ('created_at',)
     inlines = [TallyVendorAnalyzedProductInline]
+    autocomplete_fields = ('organization',)
+
     fieldsets = (
         (None, {
             'fields': ('selected_bill', 'vendor', 'bill_no', 'bill_date', 'note')
@@ -178,6 +264,12 @@ class TallyVendorAnalyzedBillAdmin(admin.ModelAdmin):
         }),
     )
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if not request.user.is_superuser:
+            qs = qs.filter(organization__in=request.user.organizations.all())
+        return qs
+
 
 class TallyVendorBillAdmin(admin.ModelAdmin):
     list_display = ('bill_munshi_name', 'status', 'file_type', 'uploaded_by', 'organization', 'display_file', 'created_at')
@@ -187,11 +279,16 @@ class TallyVendorBillAdmin(admin.ModelAdmin):
     fields = ('bill_munshi_name', 'file', 'file_type', 'status', 'process', 'uploaded_by', 'organization', 'analysed_data', 'created_at', 'updated_at')
     autocomplete_fields = ('uploaded_by', 'organization')
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if not request.user.is_superuser:
+            qs = qs.filter(organization__in=request.user.organizations.all())
+        return qs
+
     def display_file(self, obj):
         if obj.file:
             return format_html('<a href="{}" target="_blank">View File</a>', obj.file.url)
         return "-"
-
     display_file.short_description = "File"
 
 
@@ -203,11 +300,16 @@ class TallyExpenseBillAdmin(admin.ModelAdmin):
     fields = ('bill_munshi_name', 'file', 'file_type', 'status', 'process', 'uploaded_by', 'organization', 'analysed_data', 'created_at', 'updated_at')
     autocomplete_fields = ('uploaded_by', 'organization')
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if not request.user.is_superuser:
+            qs = qs.filter(organization__in=request.user.organizations.all())
+        return qs
+
     def display_file(self, obj):
         if obj.file:
             return format_html('<a href="{}" target="_blank">View File</a>', obj.file.url)
         return "-"
-
     display_file.short_description = "File"
 
 
@@ -224,6 +326,8 @@ class TallyExpenseAnalyzedBillAdmin(admin.ModelAdmin):
     search_fields = ('bill_no', 'vendor__name', 'selected_bill__bill_munshi_name', 'voucher')
     readonly_fields = ('created_at',)
     inlines = [TallyExpenseAnalyzedProductInline]
+    autocomplete_fields = ('organization',)
+
     fieldsets = (
         (None, {
             'fields': ('selected_bill', 'vendor', 'voucher', 'bill_no', 'bill_date', 'note')
@@ -236,9 +340,29 @@ class TallyExpenseAnalyzedBillAdmin(admin.ModelAdmin):
         }),
     )
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if not request.user.is_superuser:
+            qs = qs.filter(organization__in=request.user.organizations.all())
+        return qs
+
+
+class StockItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent', 'unit', 'category', 'organization', 'created_at')
+    list_filter = ('organization', 'category', 'gst_applicable', 'created_at')
+    search_fields = ('name', 'item_code', 'alias', 'organization__name')
+    readonly_fields = ('created_at', 'updated_at')
+    autocomplete_fields = ('organization',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if not request.user.is_superuser:
+            qs = qs.filter(organization__in=request.user.organizations.all())
+        return qs
+
 
 # Register models with the admin site
-admin.site.register(StockItem)
+admin.site.register(StockItem, StockItemAdmin)
 admin.site.register(ParentLedger, ParentLedgerAdmin)
 admin.site.register(Ledger, LedgerAdmin)
 admin.site.register(TallyConfig, TallyConfigAdmin)
@@ -246,4 +370,3 @@ admin.site.register(TallyVendorBill, TallyVendorBillAdmin)
 admin.site.register(TallyVendorAnalyzedBill, TallyVendorAnalyzedBillAdmin)
 admin.site.register(TallyExpenseBill, TallyExpenseBillAdmin)
 admin.site.register(TallyExpenseAnalyzedBill, TallyExpenseAnalyzedBillAdmin)
-admin.site.register(TallyExpenseAnalyzedProduct)
