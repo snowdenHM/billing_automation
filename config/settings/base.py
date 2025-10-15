f8dcc394dfbfcd564d20c6d4a69729db20d75299
Heramb <heramb1008@gmail.com>
2025-10-15 23:27:52 +0530

    Auth Token Validity increase

--- config/settings/base.py
+++ config/settings/base.py
@@ -253,7 +253,7 @@ CSRF_COOKIE_SECURE = env.bool("CSRF_COOKIE_SECURE", default=False)
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
